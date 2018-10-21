5d8a7e53d6352ee6cdeed6a2e30c863dd83f06f0
tkeeling <tylerwkeeling@gmail.com>
2018-10-20 19:36:15 -0700

    Implemented AdjacencyListGraph

--- src/main/java/ca/ubc/ece/cpen221/mp2/graph/AdjacencyListGraph.java
+++ src/main/java/ca/ubc/ece/cpen221/mp2/graph/AdjacencyListGraph.java
@@ -1,8 +1,9 @@
 package ca.ubc.ece.cpen221.mp2.graph;
 
 import ca.ubc.ece.cpen221.mp2.core.Vertex;
+import ca.ubc.ece.cpen221.mp2.core.Graph;
 
-import java.util.List;
+import java.util.*;
 
 /******************************************************************************
  *  Dependencies: Graph.java Vertex.java
@@ -12,7 +13,17 @@ import java.util.List;
  ******************************************************************************/
 
 
-public class AdjacencyListGraph  {
+public class AdjacencyListGraph implements Graph, Comparator {
+
+    Map<Vertex, Set<Vertex>> adjacencyList = new HashMap<Vertex, Set<Vertex>>();
+
+
+
+    public AdjacencyListGraph() { }
+
+    public AdjacencyListGraph(List<Vertex> inputList) {
+
+    }
 
     /**
      * Adds a vertex to the graph.
@@ -20,7 +31,9 @@ public class AdjacencyListGraph  {
      * Precondition: v is not already a vertex in the graph
      * </p>
      */
-    public void addVertex(Vertex v);
+    public void addVertex(Vertex v) {
+        adjacencyList.put(v,new HashSet<Vertex>());
+    }
 
     /**
      * Adds an edge from v1 to v2.
@@ -28,7 +41,16 @@ public class AdjacencyListGraph  {
      * Precondition: v1 and v2 are vertices in the graph
      * </p>
      */
-    public void addEdge(Vertex v1, Vertex v2);
+    public void addEdge(Vertex v1, Vertex v2) {
+        Set<Vertex> v1set = adjacencyList.get(v1);
+        v1set.add(v2);
+        adjacencyList.put(v1, v1set);
+
+        Set<Vertex> v2set = adjacencyList.get(v2);
+        v2set.add(v1);
+        adjacencyList.put(v2, v2set);
+
+    }
 
     /**
      * Check if there is an edge from v1 to v2.
@@ -39,7 +61,13 @@ public class AdjacencyListGraph  {
      * Postcondition: return true iff an edge from v1 connects to v2
      * </p>
      */
-    public boolean edgeExists(Vertex v1, Vertex v2);
+    public boolean edgeExists(Vertex v1, Vertex v2) {
+        if (adjacencyList.get(v1).contains(v2) && adjacencyList.get(v2).contains((v1))) {
+            return true;
+        }
+
+        return false;
+    }
 
     /**
      * Get an array containing all vertices adjacent to v.
@@ -53,7 +81,12 @@ public class AdjacencyListGraph  {
      * iff v has no downstream neighbors.
      * </p>
      */
-    public List<Vertex> getNeighbors(Vertex v);
+    public List<Vertex> getNeighbors(Vertex v) {
+        List<Vertex> returnlist = new ArrayList<Vertex>();
+        returnlist.addAll(adjacencyList.get(v));
+        return returnlist;
+
+    }
 
     /**
      * Get all vertices in the graph.
@@ -63,5 +96,26 @@ public class AdjacencyListGraph  {
      * This method should return a list of size 0 iff the graph has no vertices.
      * </p>
      */
-    public List<Vertex> getVertices();
+    public List<Vertex> getVertices() {
+        List<Vertex> returnlist = new ArrayList<Vertex>();
+        for(Map.Entry<Vertex,Set<Vertex>> vertex : adjacencyList.entrySet()) {
+            returnlist.add(vertex.getKey());
+        }
+
+        Comparator compare = new AdjacencyListGraph();
+        returnlist.sort(compare);
+
+        return returnlist;
+    }
+
+    /**
+     * Compares vertices alphabetically
+     * @param v1: Type Vertex
+     * @param v2: Type vertex
+     * @return normal comparator function
+     */
+    @Override
+    public int compare(Object v1, Object v2) {
+        return ((Vertex) v1).getLabel().compareTo(((Vertex) v2).getLabel());
+    }
 }
--- src/test/java/ca/ubc/ece/cpen221/mp2/graph/GraphTests.java
+++ src/test/java/ca/ubc/ece/cpen221/mp2/graph/GraphTests.java
@@ -1,5 +1,41 @@
 package ca.ubc.ece.cpen221.mp2.graph;
 
+import ca.ubc.ece.cpen221.mp2.core.Graph;
+import ca.ubc.ece.cpen221.mp2.core.Vertex;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.*;
+
+
+import static org.junit.Assert.*;
+
 public class GraphTests {
-    // possible location for tests
+    @BeforeClass
+    public static void setup() {
+
+    }
+
+    @Test
+    public void test1() {
+        Graph listgraph = new AdjacencyListGraph();
+        listgraph.addVertex(new Vertex("APPLE"));
+        listgraph.addVertex(new Vertex("ABBLE"));
+        listgraph.addVertex(new Vertex("AB"));
+        listgraph.addVertex(new Vertex("Zope"));
+        listgraph.addVertex(new Vertex("apple"));
+        listgraph.addVertex(new Vertex("apPle"));
+        listgraph.addVertex(new Vertex("Lemon"));
+
+
+
+
+
+        System.out.println(listgraph.getVertices());
+
+    }
+
+
 }
