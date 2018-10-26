a401339d8bf5214df5c7b17916c0e1ea36dde13f
Alistair <alistairbbryan@gmail.com>
2018-10-25 19:56:19 -0700

    Merge branch 'master' of https://github.com/CPEN-221-2018/mp2-TyKeeling
    # Conflicts:
    #	src/main/java/ca/ubc/ece/cpen221/mp2/graph/AdjacencyMatrixGraph.java

--- src/main/java/ca/ubc/ece/cpen221/mp2/graph/Algorithms.java
+++ src/main/java/ca/ubc/ece/cpen221/mp2/graph/Algorithms.java
@@ -23,35 +23,59 @@ public class Algorithms {
 	 *
 	 * You should write the specs for this and all other methods.
 	 *
-	 * @param graph
-	 * @param a
-	 * @param b
-	 * @return
+	 * @param graph, an undirected and unweighted object that implementats graph. Must contain
+	 * @param a, a Vertex that must be in graph.
+	 * @param b, a Vertex that must be in graph.
+	 * @return the shortest distance between a and b, taking each edge to be length 1.
+	 * @throws IllegalArgumentException if there is no path from a to b.
+	 * @throws IllegalArgumentException if a or b are not in graph
 	 */
 	public static int shortestDistance(Graph graph, Vertex a, Vertex b) {
-		Map<Vertex,Integer> distances = new HashMap<>();
+		Map<Vertex, Integer> distances = new HashMap<>();
 		Queue<Vertex> workingQueue = new LinkedBlockingQueue<>();
 		Vertex workingVertex = a;
+		List<Vertex> vertices = new ArrayList<>();
+		boolean bFound = false;
 
-		for(Vertex v : graph.getVertices()){
+		if (!vertices.contains(a) || !vertices.contains(b)) {
+			throw new IllegalArgumentException("One or more vertices not in graph.");
+		}
+
+		if (a.equals(b)) {
+			return 0;
+		}
+
+		for (Vertex v : graph.getVertices()) {
 			distances.put(v, -1);
 		}
-		distances.put(a, 0);
 
+		distances.put(a, 0);
 		workingQueue.add(a);
-		while(!workingQueue.isEmpty() && !workingVertex.equals(b)) {
+
+		while (!workingQueue.isEmpty() && !bFound) {
 			for (Vertex neighbor : graph.getNeighbors(workingVertex)) {
-				if(distances.get(neighbor) == -1){
+				if (distances.get(neighbor) == -1) {
 					distances.put(neighbor, 2 + distances.get(workingVertex));
+					if (neighbor.equals(b)) {
+						bFound = true;
+						break;
+					}
 					workingQueue.add(neighbor);
 				}
 			}
 
 			workingVertex = workingQueue.poll();
+			if (workingVertex.equals(b)) {
+				bFound = true;
+			}
 		}
 
-		return distances.get(b);
+		if (!bFound) {
+			throw new IllegalArgumentException("No path between a and b");
+		}
+	return distances.get(b);
 	}
+
 
 	/**
 	 * Perform a complete depth first search of the given
@@ -63,8 +87,9 @@ public class Algorithms {
 	 * returned set should correspond to the number of graph
 	 * vertices).
 	 *
-	 * @param
-	 * @return
+	 * @param graph, the graph through which we wish to search. Must contain at least one vertex
+	 * @return a Set of Lists, with each List indicating the chronological order at which vertices are reached.
+	 *         Each List corresponds to a different starting Vertex.
 	 */
 	public static Set<List<Vertex>> depthFirstSearch(Graph graph) {
 		Set<List<Vertex>> allResults = new HashSet<>();
@@ -108,8 +133,9 @@ public class Algorithms {
 	 * returned set should correspond to the number of graph
 	 * vertices).
 	 *
-	 * @param
-	 * @return
+	 * @param graph, the graph through which we wish to search. Must contain at least one vertex
+	 * @return a Set of Lists, with each List indicating the chronological order at which the vertices are reached.
+	 *         Each List corresponds to a different starting Vertex.
 	 */
 	public static Set<List<Vertex>> breadthFirstSearch(Graph graph) {
 		Set<List<Vertex>> allResults = new HashSet<>();
@@ -141,53 +167,109 @@ public class Algorithms {
 	}
 
 	/**
-	 * You should write the spec for this method
+	 * Finds the center vertex of a graph, with the center defined as the vertex with the smallest eccentricity.
+	 * A vertex's eccentricity is the defined as the greatest distance between that vertex and any other in the graph
+	 * and to be infinite if the vertex is not connected to any other.
+	 *
+	 * @param graph, the graph. Must have at least one connection between vertices.
+	 * @returns Vertex, the vertex with the lowest eccentricity. If multiple vertices have the eccentricity,
+	 * 			 returns the vertex with the smallest ID lexicographically.
 	 */
 	 public static Vertex center(Graph graph) {
-		 int thisMaxDistance = -1;
-		 int bestMaxDistance = -1;
-		 int testDistance;
-		 Vertex center = null;
+		 Map<Vertex,Integer> distances = new HashMap<>();
+		 Queue<Vertex> workingQueue = new LinkedBlockingQueue<>();
+		 Vertex workingVertex;
 		 List<Vertex> vertices = graph.getVertices();
+		 Vertex center = vertices.get(0);
+		 int minMax = 0;
+		 boolean isInitialized = false;
+		 int localMax;
 
-		 for(int i = 0; i < vertices.size(); i++){
-		 	for(int j = 0; j < vertices.size(); j++){
-		 		if(i != j){
-		 			testDistance = shortestDistance(graph, vertices.get(i), vertices.get(j));
-		 			if(thisMaxDistance < testDistance){
-		 				thisMaxDistance = testDistance;
-					}
-				}
-			}
-			if(thisMaxDistance < bestMaxDistance){
-				bestMaxDistance = thisMaxDistance;
-				center = vertices.get(i);
-			}
+		 for(Vertex v : vertices){
+			 distances.put(v, -1);
+		 }
+		 for(Vertex vStart : vertices) {
+		 	 if(!graph.getNeighbors(vStart).isEmpty()) {
+				 distances.put(vStart, 0);
+
+				 workingQueue.add(vStart);
+				 workingVertex = vStart;
+				 while (!workingQueue.isEmpty()) {
+
+					 for (Vertex neighbor : graph.getNeighbors(workingVertex)) {
+						 if (distances.get(neighbor) == -1) {
+							 distances.put(neighbor, 2 + distances.get(workingVertex));
+							 workingQueue.add(neighbor);
+						 }
+					 }
+
+					 workingVertex = workingQueue.poll();
+				 }
+
+				 localMax = Collections.max(distances.values());
+
+				 if (!isInitialized) {
+					 minMax = localMax;
+					 isInitialized = true;
+				 } else if (localMax < minMax) {
+					 minMax = localMax;
+					 center = vStart;
+				 }
+			 }
 		 }
 		 return center;
 	 }
 
 	 /**
-	  * You should write the spec for this method
-		*/
+	  * Determines the diameter of a graph, with the diameter defined as the maximum finite distance between
+	  * two vertices on the graph. If there is no such finite distance, the diameter is infinite and the
+	  * function returns Integer.MAX_VALUE.
+	  *
+	  * @param graph, graph with at least one vertex
+	  * @return diameter, the greatest finite distance between any 2 vertices in graph. Returns Integer.MAX_VAlUE if
+	  * 		 no such finite distance exists.
+	  */
 		public static int diameter(Graph graph) {
-			int thisMaxDistance = -1;
-			int bestMaxDistance = -1;
-			int testDistance;
+			Map<Vertex,Integer> distances = new HashMap<>();
+			Queue<Vertex> workingQueue = new LinkedBlockingQueue<>();
+			Vertex workingVertex;
 			List<Vertex> vertices = graph.getVertices();
+			int diameter = Integer.MAX_VALUE;
+			int localMax;
+			boolean finiteDiameter = false;
 
-			for(int i = 0; i < vertices.size() - 1; i++){
-				for(int j = i+1; j < vertices.size(); j++){
-					testDistance = shortestDistance(graph, vertices.get(i), vertices.get(j));
-					if(thisMaxDistance < testDistance){
-						thisMaxDistance = testDistance;
+			for(Vertex v : vertices){
+				distances.put(v, -1);
+			}
+			for(Vertex vStart : vertices) {
+				if (!graph.getNeighbors(vStart).isEmpty()) {
+					distances.put(vStart, 0);
+
+					workingQueue.add(vStart);
+					workingVertex = vStart;
+					while (!workingQueue.isEmpty()) {
+
+						for (Vertex neighbor : graph.getNeighbors(workingVertex)) {
+							if (distances.get(neighbor) == -1) {
+								distances.put(neighbor, 2 + distances.get(workingVertex));
+								workingQueue.add(neighbor);
+							}
 						}
+
+						workingVertex = workingQueue.poll();
 					}
-				if(thisMaxDistance < bestMaxDistance){
-					bestMaxDistance = thisMaxDistance;
+
+					localMax = Collections.max(distances.values());
+
+					if(!finiteDiameter){
+						diameter = localMax;
+						finiteDiameter = true;
+					}
+					else if (localMax > diameter) {
+						diameter = localMax;
+					}
 				}
 			}
-			return bestMaxDistance;
+			return diameter;
 		}
-
 }
